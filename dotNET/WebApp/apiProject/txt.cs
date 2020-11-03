9367aa77224e29af45c3459cf1441d86a02c266f
myName <aguiar_moises>
2020-11-03 10:15:11 -0400

    Configuracion de servicio MVC

--- dotNET/WebApp/apiProject/txt.cs
+++ dotNET/WebApp/apiProject/txt.cs
@@ -1,5 +1,12 @@
 // 1) dotnet new web --name apiProject
-// 1) dotnet new web --name apiProject
+
+// 2) Configurar servicio MVC
+// public void ConfigureServices(IServiceCollection services)
+// {
+//     services.addMvc();
+// }
+
+// 3) dotnet new web --name apiProject
 // 1) dotnet new web --name apiProject
 // 1) dotnet new web --name apiProject
 // 1) dotnet new web --name apiProject
