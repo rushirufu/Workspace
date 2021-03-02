90c4c5e4033d958c5dcb1ccbb13f9b24b503fcb9
NoteBook <aguiar_moises@hotmail.com>
2021-03-02 11:08:01 -0400

    creardor de projectos django

---
+++ Python/Django/run.bat
@@ -0,0 +1,16 @@
+cd c:\
+mkdir projectOne
+cd projectOne
+mkdir frontend
+mkdir backend
+cd backend
+python -m venv myenviroment
+cd myenviroment
+cd scripts
+activate 
+pip install django
+pip install freeze
+pip install rest_framework
+freeze
+pause
+
