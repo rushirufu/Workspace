559ccba410d57ddf06f885b3e9efe7ab99fda42e
NoteBook <aguiar_moises@hotmail.com>
2021-03-04 13:37:05 -0400

    commit

--- Python/Django/run.bat
+++ Python/Django/run.bat
@@ -3,14 +3,31 @@ mkdir projectOne
 cd projectOne
 mkdir frontend
 mkdir backend
+echo %cd%
 cd backend
-python -m venv myenviroment
-cd myenviroment
-cd scripts
-activate 
+echo %cd%
+@REM python -m venv myenviroment
+@REM cd myenviroment
+@REM echo %cd%
+@REM cd Scripts
+@REM echo %cd%
+@REM start .\activate.bat 
 pip install django
 pip install freeze
-pip install rest_framework
-freeze
-pause
+pip install djangorestframework
+@REM pip freeze --local
+@REM deactivate.bat
+@REM cd ..
+@REM cd ..
+@REM REM  django-admin startporject api
+@REM TIMEOUT /T 20
+@REM PAUSE
 
+
+
+REM #pip install --upgrade pip
+REM python -m pip install --upgrade
+REM python -m pip install -r requirements.txt
+pip install django
+pip install freeze
+pip install djangorestframework
